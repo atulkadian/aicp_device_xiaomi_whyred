c1486d65a997bfdb45243f86ef9dd3f118025f16
Dan Cashman <dcashman@google.com>
2017-06-01 15:25:22 -0700

    whyred: Add BOARD_PLAT_[PUBLIC|PRIVATE]_SEPOLICY_DIR
    Move vendor policy to vendor and add a place for system extensions.
    Also add such an extension: a labeling of the qti.ims.ext service.
    
    Bug: 38151691
    Bug: 62041272
    Test: Policy binary identical before and after, except plat_service_contexts
    has new service added.
    Change-Id: Ie4e8527649787dcf2391b326daa80cf1c9bd9d2f
    
    Change-Id: I1493c4c8876c4446a1de46b39942098bf49c79f8
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>
    Signed-off-by: Atul Kadian <atul2143143@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -230,7 +230,9 @@ BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
 # We modify several neverallows, so let the build proceed
 ifneq ($(TARGET_BUILD_VARIANT),user)
