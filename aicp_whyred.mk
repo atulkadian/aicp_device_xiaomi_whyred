bca5516ec12364ceeaa881cfc328f4fcf9980431
AtulKadian <atul2143143@gmail.com>
2019-03-06 16:27:59 +0000

    whyred: add device maintainers
    Change-Id: Iaff1240046392f62c5f2057540276697d8fe6628

--- aicp_whyred.mk
+++ aicp_whyred.mk
@@ -45,3 +45,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := xiaomi/whyred/whyred:8.1.0/OPM1.171019.011/V9.5.11.0.OEIMIFA:user/release-keys
 
 TARGET_VENDOR := Xiaomi
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Atul Kadian (Saabu)"
